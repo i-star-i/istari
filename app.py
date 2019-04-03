caa934bf278b83ae92b7bcc3d3b73191f82eb8e8
Sorrel Harriet <s.harriet@gold.ac.uk>
2019-04-03 10:57:31 +0100

    updated app with initial structure and boilerplate template

--- app.py
+++ app.py
@@ -4,15 +4,12 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-#client = MongoClient('eab092bcc2ca', 27017)
-client = MongoClient(
-            os.environ['MONGODB_HOST'],
-                27017)
+client = MongoClient(os.environ['MONGODB_HOST'],27017)
 db = client.profiles
 
 @app.route('/')
-def hello():
-    return "hello world from a Docker container"
+def home():
+    return render_template('home.html', page='home')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -25,19 +22,25 @@ def register():
             'github_username': request.form['github_username']
         }
         db.profiles.insert_one(profile_doc)
-        return redirect(url_for('test'))
+        return redirect(url_for('confirm'))
     else:
         return render_template('register.html')
 
+@app.route('/confirm')
+def confirm():
+    return "#TODO confirmation of registration page"
 
-@app.route('/test')
-def test():
-    """ #TODO: delete this
-        dummy view to check stuff is connected
+@app.route('/search')
+def search():
+    """ #TODO: search view renders search form
     """
+    return render_template('search.html')
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
     _profiles = db.profiles.find()
     profiles = [profile for profile in _profiles]
-    return render_template('test.html', profiles=profiles)
+    return render_template('results.html', profiles=profiles)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
